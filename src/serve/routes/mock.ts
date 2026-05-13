88c85d755390f5cd4cafb86338a893460dab9b60
yanglee2421 <yanglee2421@gmail.com>
2026-05-13 16:24:10 +0800

    fix

--- src/serve/routes/mock.ts
+++ src/serve/routes/mock.ts
@@ -12,56 +12,98 @@ const schema = z.object({
   zh: z.string(),
 });
 
+interface Item {
+  //单号
+  DH: string;
+  //轴号
+  ZH: string;
+  //轴型
+  ZX: string;
+  //收入单位
+  SRDW: string;
+  //收入原因
+  SRYY: string;
+  //车轴制造单位
+  CZZZDW: string;
+  //车轴制造日期
+  CZZZRQ: string;
+  //首次组装单位
+  SCZZDW: string;
+  //首次组装日期
+  SCZZRQ: string;
+  //末次组装单位
+  MCZZDW: string;
+  //末次组装日期
+  MCZZRQ: string;
+  //左轴
+  ZZC: boolean;
+  //右轴承
+  YZC: boolean;
+}
+
 mock.post("/work", zValidator("json", schema), (c) => {
   const bodyJson = c.req.valid("json");
   const { dh, zh } = bodyJson;
 
   const date = dayjs().format("YYYY-MM-DD");
 
+  const items: Item[] = [];
+
   if (dh) {
-    return c.json([
-      {
-        DH: dh,
-        ZH: "123456",
-        ZZSJ: date,
-        ZZDW: "111",
-        SCZZ: date,
-        SCDW: "333",
-        MCZZ: "444",
-        MCDW: "555",
-        ZZC: true,
-        YZC: false,
-      },
-    ]);
+    items.push({
+      DH: dh,
+      ZH: "38254",
+      ZX: "RD2",
+      SRDW: "003",
+      SRYY: "",
+      CZZZDW: "005",
+      CZZZRQ: date,
+      SCZZDW: "009",
+      SCZZRQ: date,
+      MCZZDW: "007",
+      MCZZRQ: date,
+      ZZC: false,
+      YZC: false,
+    });
+
+    return c.json(items);
   }
 
   if (zh) {
-    return c.json([
+    items.push(
       {
-        DH: "96164616156",
+        DH: "DH001",
         ZH: zh,
-        ZZSJ: date,
-        ZZDW: "111",
-        SCZZ: date,
-        SCDW: "333",
-        MCZZ: "444",
-        MCDW: "555",
-        ZZC: true,
+        ZX: "RD2",
+        SRDW: "003",
+        SRYY: "",
+        CZZZDW: "005",
+        CZZZRQ: date,
+        SCZZDW: "009",
+        SCZZRQ: date,
+        MCZZDW: "007",
+        MCZZRQ: date,
+        ZZC: false,
         YZC: false,
       },
       {
-        DH: "132165464",
+        DH: "DH002",
         ZH: zh,
-        ZZSJ: date,
-        ZZDW: "111",
-        SCZZ: date,
-        SCDW: "333",
-        MCZZ: "444",
-        MCDW: "555",
+        ZX: "RE2B",
+        SRDW: "001",
+        SRYY: "",
+        CZZZDW: "002",
+        CZZZRQ: date,
+        SCZZDW: "003",
+        SCZZRQ: date,
+        MCZZDW: "004",
+        MCZZRQ: date,
         ZZC: true,
-        YZC: false,
+        YZC: true,
       },
-    ]);
+    );
+
+    return c.json(items);
   }
 
   throw new Error("dh或zh必须提供其中至少一个");
