973c063492bc7cb9efd1755387e1bccd1ea050ff
yanglee2421 <yanglee2421@gmail.com>
2026-05-15 08:19:27 +0800

    chore: test heartbeat api

--- src/dev.ts
+++ src/dev.ts
@@ -1,12 +1,14 @@
 import "./serve/index.ts";
 
-const dev = async () => {
-  const BASE_URL = "127.0.0.1";
-  //   const BASE_URL = "47.103.157.15";
+// const BASE_URL = "127.0.0.1";
+const BASE_URL = "47.103.157.15";
+
+export const dev = async () => {
+  const url = new URL("/hmis/work", `http://${BASE_URL}:5003`);
+
   const headers = new Headers();
   headers.set("Content-Type", "application/json;charset=utf-8");
 
-  const url = new URL("/hmis/work", `http://${BASE_URL}:5003`);
   const res = await fetch(url, {
     method: "POST",
     body: JSON.stringify({
@@ -20,4 +22,14 @@ const dev = async () => {
   console.log(data);
 };
 
-dev();
+export const heartbeat = async () => {
+  const url = new URL("/hmis/heartbeat", `http://${BASE_URL}:5003`);
+
+  const res = await fetch(url, {
+    method: "POST",
+  });
+
+  const data = await res.json();
+
+  console.log(data);
+};
