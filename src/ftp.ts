8739c89b6e611cf482e03f9e37668023a3746104
yanglee2421 <yanglee2421@gmail.com>
2026-06-30 10:31:45 +0800

    feat: Add ftp server

---
+++ src/ftp.ts
@@ -0,0 +1,20 @@
+import FtpServer from "@electerm/ftp-srv";
+import path from "node:path";
+import url from "node:url";
+
+const server = new FtpServer({
+  url: "ftp://127.0.0.1:21",
+  anonymous: false,
+});
+
+server.on("login", ({ username, password }, resolve, reject) => {
+  if (username === "admin" && password === "secret") {
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const root = path.resolve(__dirname, "./");
+    return resolve({ root });
+  }
+  return reject(new Error("Invalid credentials"));
+});
+
+await server.listen();
