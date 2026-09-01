e8622667802228d0167230c9a85e843e113e1f42
yanglee2421 <yanglee2421@gmail.com>
2026-09-01 08:13:11 +0800

    feat: add hxzy post mock api

--- src/serve/routes/hxzy.ts
+++ src/serve/routes/hxzy.ts
@@ -17,3 +17,40 @@ hxzyRouter.post(
     });
   },
 );
+
+hxzyRouter.post(
+  "/lzjx/dx/csbts/device_api/csbts/api/saveData",
+  subMiddleware,
+  async (c) => {
+    const body = await c.req.json();
+
+    console.log(body);
+
+    return c.json({
+      code: "200",
+      msg: "数据上传成功",
+    });
+  },
+);
+
+hxzyRouter.get("/lzjx/dx/csbts/device_api/csbts/api/getDate", (c) => {
+  return c.json({
+    code: "200",
+    msg: "数据读取成功",
+    data: [
+      {
+        CZZZDW: "048",
+        CZZZRQ: "2009-10",
+        MCZZDW: "131",
+        MCZZRQ: "2018-07-09 00:00:00",
+        SCZZDW: "131",
+        SCZZRQ: "2018-07-09 00:00:00",
+        DH: "91022070168",
+        ZH: "67444",
+        ZX: "RE2B",
+        SRYY: "厂修",
+        SRDW: "588",
+      },
+    ],
+  });
+});
